7049750d1444d9ab01aa716fd15f67bd40a37916
11lization <11lization@naver.com>
2022-05-21 17:39:26 +0900

    flip_a_word

---
+++ flip_a_word/flip_a_word.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string str;
+stack<char> word;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int caseNum;
+
+    cin >> caseNum;
+    cin.ignore();
+
+    while(caseNum--){
+        getline(cin, str);
+        for(int i = 0; i < str.length(); i++){
+            if(str[i] != ' '){
+                word.push(str[i]);
+            }
+            if(str[i] == ' ' || i == str.length() - 1){
+                while(!word.empty()){
+                    cout << word.top();
+                    word.pop();
+                }
+                cout << ' ';
+            }
+        }
+        cout << "\n";
+    }
+}
