b32156ff88fd0cc86f98402687cfb5b61131be37
11lization <11lization@naver.com>
2022-05-23 23:38:28 +0900

    flip_a_word2

---
+++ flia_a_word2/flip_a_word2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+string str;
+stack<char> word;
+
+int main(){
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    bool intag = false;
+
+    getline(cin, str);
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] == '<')
+            intag = true;
+        if(str[i] == '>'){
+            intag = false;
+            cout << str[i];
+            continue;
+        }
+        if(intag){
+            cout << str[i];
+            continue;
+        }
+        if(str[i] != ' '){
+            word.push(str[i]);
+        }
+        if(str[i] == ' ' || str[i + 1] == '<' ||i == str.length() - 1){
+            while(!word.empty()){
+                cout << word.top();
+                word.pop();
+            }
+            if(str[i + 1] != '<')
+                cout << ' ';
+        }
+    }
+    
+    cout << "\n";
+
+}
